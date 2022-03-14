caf2c3931856a1dc0353fc9518a02d75ee8ac453
Superjan25 <jan.scherer1@gmail.com>
2022-03-14 21:45:08 +0000

    updated index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //get inputs
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    //compute amount which will be received
+    var amount = principal * years * rate /100;
+
+    //compute year acording to input
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    //check whether principal is greater than zero
+    if (principal<1){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+
+    //return the result in result Element
+    document.getElementById("result").innerHTML = "If you deposit <span class='highlight'>"+principal+
+        "</span>,<br/>at an interest rate of <span class='highlight'>"+rate+
+        "%</span>.<br/>You will receive an amount of <span class='highlight'>"+amount+
+        "</span>,<br/>in the year <span class='highlight'>"+year+"</span><br/>";
 }
-        
+
+function updateRate()
+{
+    //show the selected amount of the range input
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
+}
